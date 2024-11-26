0b2dd7d01d7ac830e55cf8f044bffc462847e0f0
coopercox315 <70693747+coopercox315@users.noreply.github.com>
2024-11-26 17:34:28 +1030

    added 'shape' arg to load_image function and defined function for displaying images

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,12 +5,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #function to load and preprocess an image
-def load_image(img_path, max_size=400):
+def load_image(img_path, max_size=400, shape=None):
     """
     Load and preprocess an image from a given path.
     Args:
-    - imag_path (str): path to the image file.
+    - img_path (str): path to the image file.
     - max_size (int): the maximum size for resizing the image.
+    - shape (tuple): the shape to resize the image to. Input content image shape when loading style image.
     
     Returns:
     - Tensor: a tensor representing the preprocessed image.
@@ -21,6 +22,11 @@ def load_image(img_path, max_size=400):
     if max(image.size) > max_size:
         scale  = max_size / float(max(image.size))
         new_size = (int(image.size[0] * scale), int(image.size[1] * scale))
+    else:
+        new_size = image.size
+
+    if shape is not None:
+        new_size = shape
 
     #Convert image to tensor and normalize
     transform = T.Compose([
@@ -52,3 +58,20 @@ def to_numpy(tensor):
     image = np.clip(image, 0, 1)
 
     return image
+
+#function to display content and style images
+def display_images(content, style):
+    """
+    Display the content and style images.
+    Args:
+    - content (Tensor): the content image tensor.
+    - style (Tensor): the style image tensor.
+    """
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+    ax1.imshow(to_numpy(content))
+    ax2.imshow(to_numpy(style))
+    plt.show()
+
+# content = load_image("tiger.jpg")
+# style = load_image("starry.jpg", shape=content.shape[-2:])
+# display_images(content, style)
